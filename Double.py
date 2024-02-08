c516a14bdd9c74cfa5503ec95706521687e617e7
Agnussss <158853177+Agnussss@users.noreply.github.com>
2024-02-09 10:41:59 +1300

    Double 2

--- Double.py
+++ Double.py
@@ -1,7 +1,15 @@
 '''this might do something'''
 
-try:
-    x = float(input())
-    print(x*2)
-except ValueError:
-    print("number please")
+keep_asking = True
+while keep_asking == True:
+    try:
+        x = float(input("Enter a positive number: "))
+        if x>= 0:
+            keep_asking = False
+        else:
+            print("Please enter a positive number")
+
+    except ValueError:
+        print("number please")
+
+print(f"{x} doubled is {x*2}")
