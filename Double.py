63791a1ce4a85b4ee952a331973163b294ca48a9
Agnussss <158853177+Agnussss@users.noreply.github.com>
2024-02-08 14:53:20 +1300

    Create Double.py

---
+++ Double.py
@@ -0,0 +1,6 @@
+#Mathew is not very cool if you could not tell
+def MathewBluckisnotverycool():
+    x = int(input("wasgood mahomie tell us your number "))
+    print("nah i prefer the number", x*2)
+
+MathewBluckisnotverycool()
